7be33cc914573f17f0c296fb66bb53cb35073ec3
max <letnidev@gmail.com>
2019-04-16 15:49:35 +0300

    added navbar, removed dividers

---
+++ src/components/common/nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+
+const FixedMenuLayout = () => (
+  <div>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' src='https://www.shareicon.net/data/128x128/2015/09/14/101012_doge_512x512.png' style={{ marginRight: '1.5em' }} />
+          knowis
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+      </Container>
+    </Menu>
+  </div>
+)
+
+export default FixedMenuLayout;
--- src/containers/QuestionPage/index.js
+++ src/containers/QuestionPage/index.js
@@ -23,29 +23,36 @@ class QuestionPage extends Component {
         const {QuestionStore} = this.props;
 
         return (
-                QuestionStore.questions.map(question => (
+            <div>
+            <div>
+                {QuestionStore.questions.map(question => (
                     question.results.map(item => (
                         <Segment className={styles.box} vertical>
                             <Container text>
                                 <Header as='h3' className={styles.headerTitle}>{item.title}</Header>
                                 <p className={styles.content}>{item.content}</p>
-                                <Button as='a' color='yellow' size='mini'>Больше</Button>
+                                <Button as='a' color='yellow' size='mini'>Перейти до питання</Button>
                             </Container>
-                            <Divider className='header' horizontal>
                                 <p>{item.get_tags.map(
                                     tag => (
                                         <a href={tag}>
-                                            <Label as='a' color='blue'>
+                                            <Label as='a' color='blue' image>
+                                                <img src='https://static.thenounproject.com/png/99472-200.png' />
                                                 {tag}
                                             </Label>
+                                            <span>&nbsp;</span>
                                         </a>
                                     )
                                 )}</p>
-
-                            </Divider>
                         </Segment>
                     ))
                 ))
+                }
+            </div>
+                <Segment>
+                    <Button>Інші питання</Button>
+                </Segment>
+            </div>
         );
     }
 }
