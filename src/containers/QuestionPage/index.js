733312ffb0034feceacb8f994429551cb76d054c
Max <letniqq@localhost.localdomain>
2019-04-19 00:41:05 +0300

    added grid to questions

--- src/containers/QuestionPage/index.js
+++ src/containers/QuestionPage/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import QuestionService from '../../services/QuestionService';
-import {Container, Header, Segment, Button, Divider, Label} from 'semantic-ui-react'
+import {Container, Header, Segment, Button, Icon, Grid, Image, Label} from 'semantic-ui-react'
 import styles from './styles.module.scss';
 
 
@@ -36,23 +36,55 @@ class QuestionPage extends Component {
             <div>
                 {QuestionStore.questions.map(question => (
                     question.results.map(item => (
-                        <div className={styles.box} vertical>
+                        <div className={styles.box} >
                             <Container text>
                                 <Segment>
-                                <Header as='h3' className={styles.headerTitle}>{item.title}</Header>
-                                <p className={styles.content}>{item.content}</p>
-                                <Button as='a' color='yellow' size='mini'>Перейти до питання</Button>
-                                <div className={styles.tags}>{item.get_tags.map(
-                                    tag => (
-                                        <a href={tag}>
-                                            <Label as='a' color='brown' image>
-                                                <img src='https://static.thenounproject.com/png/99472-200.png' />
-                                                {tag}
-                                            </Label>
-                                            <span>&nbsp;</span>
-                                        </a>
-                                    )
-                                )}</div>
+                                    <Grid celled='internally' columns='equal'>
+                                        <Grid.Row>
+                                            <Grid.Column width={4}>
+                                                <Image src={item.image}/>
+                                                <div className={styles.tags}>
+                                                    {item.get_tags.map(
+                                                        tag => (
+                                                            <div className={styles.innerTags}>
+                                                            <a href={tag}>
+                                                                <Label as='a' color='brown' image>
+                                                                    <img src='https://static.thenounproject.com/png/99472-200.png' />
+                                                                    {tag}
+                                                                </Label>
+                                                            </a>
+                                                            </div>
+                                                        )
+                                                    )}
+                                                </div>
+                                            </Grid.Column>
+                                            <Grid.Column width={10}>
+                                                <p className={styles.content}>{item.content}</p>
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                        <Grid.Row>
+                                            <Grid.Column width={10}>
+                                                <div className={styles.button}>
+                                                <Button as='div' labelPosition='left' size='mini'>
+                                                        <Label as='a' basic color='red'>
+                                                            2048
+                                                        </Label>
+                                                    <Button icon size='mini' color='red'>
+                                                        <Icon name='like' />
+                                                    </Button>
+                                                </Button>
+                                                <Button as='div' labelPosition='left' size='mini'>
+                                                    <Label as='a' basic color='blue' >
+                                                        2048
+                                                    </Label>
+                                                    <Button icon size='mini' color='blue'>
+                                                        <Icon name='comment' />
+                                                    </Button>
+                                                </Button>
+                                                </div>
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    </Grid>
                                 </Segment>
                                 </Container>
                         </div>
