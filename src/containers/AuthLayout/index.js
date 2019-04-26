c0cce575832384251fd18a8b8218379a0c3dffa7
max <letnidev@gmail.com>
2019-04-26 18:05:13 +0300

    added change state on click

--- src/containers/AuthLayout/index.js
+++ src/containers/AuthLayout/index.js
@@ -19,31 +19,31 @@ class LoginPage extends Component {
     render() {
         const {values, errors, inProgress} = this.props.AuthStore;
     return (
-        <Grid.Column textAlign='center'>
+        <Grid.Column columns={2} textAlign='center'>
                                     <Form error={errors} onSubmit={this.handleSubmitForm}>
                                         <Form.Input
-                                            name='username' onChange={this.handleEmailChange} value={values.email}
-                                            icon='user' iconPosition='left' label='E-mail' placeholder='E-mail' type='email'
+                                            name='username' onChange={this.handleUsernameChange} value={values.email}
+                                            icon='user' iconPosition='left' label='Логін' placeholder='Логін' type='text'
                                         />
                                         <Form.Input
                                             name='password' onChange={this.handlePasswordChange} value={values.password1}
                                             icon='lock' iconPosition='left' label='Пароль' type='password' placeholder='Пароль'
                                         />
                                         <Form.Input
-                                            name='username' onChange={this.handleEmailChange} value={values.email}
-                                            icon='user' iconPosition='left' label='E-mail' placeholder='E-mail' type='email'
+                                            name='email' onChange={this.handleEmailChange} value={values.email}
+                                            icon='mail' iconPosition='left' label='E-mail' placeholder='E-mail' type='email'
                                         />
                                         <Form.Input
-                                            name='password' onChange={this.handlePasswordChange} value={values.password1}
-                                            icon='lock' iconPosition='left' label='Пароль' type='password' placeholder='Пароль'
+                                            name='password2' onChange={this.handlePasswordChange} value={values.password2}
+                                            icon='lock' iconPosition='left' label='Повторіть пароль' type='password' placeholder='Повторіть пароль'
                                         />
                                         <Message
                                             error
                                             header='Помилка'
                                             content='Деталі для входу не вірні або аккаунт не зареєстровано'
                                         />
-                                        <Button disabled={!values.email || inProgress}  color='orange' fluid size='large' >
-                                            Увійти
+                                        <Button disabled={(!values.email && !values.username) || inProgress}  color='orange' fluid size='large' >
+                                            Зареєструватися
                                         </Button>
                                     </Form>
                                 </Grid.Column>
